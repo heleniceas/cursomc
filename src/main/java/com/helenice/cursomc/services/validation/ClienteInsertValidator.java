9ef2bc7c8092bafc4b6c975bf026d2cc0b99b7c2
Helenice Araujo da Silva <helenice.silva@apoioprodesp.sp.gov.br>
2019-01-02 15:00:06 -0200

    Validacao de CPF e CNPJ

---
+++ src/main/java/com/helenice/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,52 @@
+package com.helenice.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.helenice.cursomc.domain.Cliente;
+import com.helenice.cursomc.domain.enums.TipoCliente;
+import com.helenice.cursomc.dto.ClienteNewDTO;
+import com.helenice.cursomc.repositories.ClienteRepository;
+import com.helenice.cursomc.resources.exception.FieldMessage;
+import com.helenice.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+//		Cliente aux = repo.findByEmail(objDto.getEmail());
+//		if (aux != null) {
+//			list.add(new FieldMessage("email", "Email já existente"));
+//		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
